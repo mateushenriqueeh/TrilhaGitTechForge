d6faab1a1e82f2cf25cfc00fb1752aafa52f7259
mateushenriqueeh <mateushenrique1314.mg@gmail.com>
2024-04-06 15:32:38 -0300

    Exercicio 3 resolvido

---
+++ index3.js
@@ -0,0 +1,16 @@
+
+
+let string = "Hello, World";
+
+
+console.log("Comprimento", string.length);
+
+
+
+let stringmaiuscula = string.toUpperCase();
+console.log("letras maiúsculas:", stringmaiuscula);
+
+let palavras= string.split(" ");
+console.log(palavras);
+
+

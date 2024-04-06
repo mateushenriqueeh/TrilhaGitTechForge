29446ba2ab10fe213b0606e755d7de0941a564a7
mateushenriqueeh <mateushenrique1314.mg@gmail.com>
2024-04-06 15:30:00 -0300

    Exercicio 1 trilha resolvido

---
+++ index1.js
@@ -0,0 +1,8 @@
+// 1- Declare duas variáveis, a e b, e atribua valores numéricos a elas. Crie uma terceira variável, soma, e atribua a soma de a e b a ela. Imprima o resultado no console.
+
+let a = 2
+let b = 2
+
+let soma = a+b
+
+console.log("O resultado é: ", soma)

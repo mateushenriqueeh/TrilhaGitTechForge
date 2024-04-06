ba3b85c02989de0db3a12c70dadee6e21f445ba7
mateushenriqueeh <mateushenrique1314.mg@gmail.com>
2024-04-06 15:31:32 -0300

    Exercicio 2 resolvido

---
+++ index2.js
@@ -0,0 +1,13 @@
+//2- Crie uma função chamada verificaNumero que recebe um número como parâmetro e imprime "É par" se o número for par, e "É ímpar" se for ímpar. Teste a função com diferentes números
+
+let numero = 36;
+
+if(numero % 2 ===0){
+    console.log("O numero é par")
+}else{
+    console.log("O numero é ímpar")
+}
+
+
+
+
